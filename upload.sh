b77234cbcfee7b4c2a9587dc2d84b3477d729160
AdityaJangid <eradityajangid@gmail.com>
2018-06-24 17:01:40 +0530

    added handler configuration

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 rm -rf *.zip
 zip -r userAdd.zip pymysql/ useradd.py
 aws lambda update-function-code --function-name user_add --zip-file fileb://userAdd.zip --profile=dev
+aws lambda update-function-configuration --function-name user_add --handler useradd.handler --profile=dev
